58f5e7c80606c355410c0a27843d772d2548d4e2
Tanmay Siwach <siwachtanmayus@gmail.com>
2023-11-11 18:10:43 -0800

    gcloud test

--- frontend/src/service/apiClient.js
+++ frontend/src/service/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBasePath = 'https://oneshot-404901.uc.r.appspot.com/api'
+const apiBasePath = 'https://oneshot-404901.uc.r.appspot.com/'
 
 export const getNewCompaniesList = async (userMessage) => {
     try {
