1b1606bccda575792ffa79ef552091b5837b81f7
Tanmay Siwach <siwachtanmayus@gmail.com>
2023-11-11 16:23:07 -0800

    port test

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,6 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use('/', router);
 
-app.listen(80, () => {
+app.listen(port, () => {
     console.log(`Server is running on THIS port ${port}`);
 });
