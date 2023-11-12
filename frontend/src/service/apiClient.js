4f37018ba54625ab9c4d7145e5d9a31225851577
Tanmay Siwach <siwachtanmayus@gmail.com>
2023-11-11 16:16:24 -0800

    api path

--- frontend/src/service/apiClient.js
+++ frontend/src/service/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBasePath = 'https://ec2-18-117-96-59.us-east-2.compute.amazonaws.com/'
+const apiBasePath = 'https://ec2-18-117-96-59.us-east-2.compute.amazonaws.com:8080'
 
 export const getNewCompaniesList = async (userMessage) => {
     try {
