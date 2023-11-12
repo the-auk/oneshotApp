9e37e656b370375a3221e897ca6b92a3407b539b
Tanmay Siwach <siwachtanmayus@gmail.com>
2023-11-11 17:22:02 -0800

    build

--- frontend/src/service/apiClient.js
+++ frontend/src/service/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBasePath = 'https://ec2-18-117-96-59.us-east-2.compute.amazonaws.com:8080'
+const apiBasePath = 'https://oneshot-404901.uc.r.appspot.com'
 
 export const getNewCompaniesList = async (userMessage) => {
     try {
